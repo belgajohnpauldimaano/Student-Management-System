b78a8d960bee5f799c46fa6758ed7cefce1c7414
Fernando Sanchez <fernandocsanchez5@gmail.com>
2018-05-17 19:27:41 +0800

    added content for vison mission

--- resources/views/pages/about/vision_mission.blade.php
+++ resources/views/pages/about/vision_mission.blade.php
@@ -16,7 +16,25 @@
 	</div>
     <main id="main">
 		<div class="container">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora consequatur non atque, dolorem iste numquam assumenda inventore vitae quod. Nisi porro praesentium consequuntur ad provident minus, accusamus odit aspernatur error.</p>
+			<div class="row">
+				<div class="col-md-8 mx-auto">
+					<h2>Vision</h2>
+					<p>We envision to develop morally, socially upright, and competitive Filipino citizens equipped with total quality  education and Christian formation in the service of a humane and peaceful society.</p>
+
+					<h2>Vision</h2>
+					<p>Commitment to develop morally, socially upright and globally competitive Filipino citizens.</p>
+					
+					<h2>Goals</h2>	
+					<ol>
+						<li>To provide the total Christian formation to the youth by giving them various faith avenues that will develop them to become authentic Catholic / Christians.</li>
+						<li>To provide a curriculum and instructional facilities that will develop their skills to become globally competitive Filipino citizens.</li>
+						<li>To develop a roster of excellent administrative and faculty members who will continually engage in their holistic development.</li>
+						<li>To foster an environment of social, economic and political awareness responsive to the changing needs of the society.</li>
+					</ol>			
+					<br>
+					<br>
+				</div>
+			</div>
 		</div>
     </main>
 @endsection
