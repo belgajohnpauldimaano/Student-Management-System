4724cc6f6fdde75ca6d23d8876ed35aafb13527d
Fernando Sanchez <fernandocsanchez5@gmail.com>
2018-06-17 01:31:13 +0800

    update

--- resources/views/pages/about/hymn.blade.php
+++ resources/views/pages/about/hymn.blade.php
@@ -16,7 +16,40 @@
 	</div>
     <main id="main">
 		<div class="container">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora consequatur non atque, dolorem iste numquam assumenda inventore vitae quod. Nisi porro praesentium consequuntur ad provident minus, accusamus odit aspernatur error.</p>
+			<div class="row">
+				<div class="col-md-8">
+					<div class="text-center">
+						<p>
+							Comrades we gather, let’s get together <br>
+							As we greet our loving Alma Mater <br>
+							School mates and teachers, parents and founders
+						</p>
+
+						<p>
+							For this school we’ll bind ourselves together <br>
+							Onward we go on, forward we march on <br>
+							Long live our school named after Saint John <br>
+							Guide us patron Saint John <br>
+							Lead the way to march on <br>
+							To the light of learning you have shown.
+						</p>
+
+						<p>
+							Blessed be the day it is founded <br>
+							Saint John Academy its name will live forever <br>
+							Through its portals we shall pass by <br>
+							For better life and greater glory to remember <br>
+							Work and play in school we are happy <br>
+							No work undone all tasks we ever try <br>
+							We bow our heads Saint John Academy <br>
+							We praise our patron forever.
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+                    @include('pages.about.partials.sidebar')
+				</div>
+			</div>
 		</div>
     </main>
 @endsection
