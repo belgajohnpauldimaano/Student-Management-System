73d2171a2f0b10ab207d5eebcf16e6786673fe46
Nikko Aldrich Monroe <monroenikkoaldrich@yahoo.com>
2019-07-13 08:58:48 +0800

    updated version of website

--- resources/views/control_panel_student/grade_sheet/partials/print.blade.php
+++ resources/views/control_panel_student/grade_sheet/partials/print.blade.php
@@ -103,7 +103,8 @@
                 <p class="heading1">Region III</p>
                 <p class="heading1">Division of Bataan</p>
                 <br/>
-                <h2 class="heading2 heading2-title">Saint John Academy</h2>
+                <h2 class="heading2 heading2-title">Saint John's Academy, Inc</h2>
+                <p class="heading2 heading2-subtitle"><b>Formerly Saint John Academy</b></p>
                 <p class="heading2 heading2-subtitle">Dinalupihan, Bataan</p>
                 <br/>
                 <p class="report-progress m0">REPORT ON LEARNING PROGRESS AND ACHIEVEMENT</p>
@@ -206,6 +207,8 @@
                                     enrollments.class_details_id as cid,
                                     enrollments.attendance_first,
                                     enrollments.attendance_second,
+                                    enrollments.j_lacking_unit,
+                                    enrollments.s1_lacking_unit,
                                     class_details.grade_level,
                                     class_subject_details.id as class_subject_details_id,
                                     class_subject_details.class_days,
@@ -477,33 +480,33 @@
                         <td style="border: 0">Grading Scale</td>
                         <td style="border: 0">Remarks</td>                
                     </tr>
-
+    
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Outstanding</td>
-                        <td style="border: 0">90-100</td>
+                        <td style="border: 0">With Highest Honors</td>
+                        <td style="border: 0">98-100</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
-
+    
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Very Satisfactory</td>
-                        <td style="border: 0">85-89</td>
+                        <td style="border: 0">With High Honors</td>
+                        <td style="border: 0">95-97</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
-
+    
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Satisfactory</td>
-                        <td style="border: 0">80-84</td>
+                        <td style="border: 0">With Honors</td>
+                        <td style="border: 0">90-94</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
-
+    
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Fairly Satisfactory</td>
+                        <td style="border: 0">Passed</td>
                         <td style="border: 0">75-79</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
-
+    
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Did Not Meet expectations</td>
+                        <td style="border: 0">Failed</td>
                         <td style="border: 0">Below 75</td>
                         <td style="border: 0">Failed</td>                
                     </tr>
@@ -535,7 +538,7 @@
                     </tr>
 
                     <tr style="margin-top: .5em">
-                        <td colspan="3" style="border: 0">Lacking units in:__________________________</td>                
+                        <td colspan="3" style="border: 0">Lacking units in:___<u>{{ $Enrollment[0]->s1_lacking_unit }}</u>____</td>                
                     </tr>
                     
                     <tr style="margin-top: .5em">
@@ -624,6 +627,7 @@
                                 enrollments.class_details_id as cid,
                                 enrollments.attendance_first,
                                 enrollments.attendance_second,
+                                enrollments.s2_lacking_unit,
                                 class_details.grade_level,
                                 class_subject_details.id as class_subject_details_id,
                                 class_subject_details.class_days,
@@ -728,17 +732,15 @@
                                             @if(round($StudentEnrolledSubject1->thi_g) != 0 && round($StudentEnrolledSubject1->fou_g) != 0)
                                                 {{-- {{$$final_ave && $general_avg >= 0 ? round($general_avg) : '' }} --}}
                                                 <?php
-                                                 $totalsum = 0;
-                                                 $count_subjects1 = \App\StudentEnrolledSubject::where('enrollments_id', $Enrollment[0]->enrollment_id)
+                                                    $totalsum = 0;
+                                                    $count_subjects1 = \App\StudentEnrolledSubject::where('enrollments_id', $Enrollment[0]->enrollment_id)
                                                     ->where('sem', 2)->where('status', '!=', 0)->count();
                                                 ?>
                                                 @foreach($StudentEnrolledSubject as $key => $data)
-                                                <?php
-                                                    
+                                                <?php                                                    
                                                     round($final_ave = (round($data->thi_g) + round($data->fou_g)) / 2);                                                                                                
                                                     $totalsum+= round($final_ave) / $count_subjects1 ;   
-                                                    // echo $sum;
-                                                                                                         
+                                                    // echo $sum;                                                                                                         
                                                 ?>
                                                 @endforeach
                                                 <?php
@@ -894,31 +896,31 @@
                 </tr>
 
                 <tr style="margin-top: .5em">
-                    <td style="border: 0">Outstanding</td>
-                    <td style="border: 0">90-100</td>
+                    <td style="border: 0">With Highest Honors</td>
+                    <td style="border: 0">98-100</td>
                     <td style="border: 0">Passed</td>                
                 </tr>
 
                 <tr style="margin-top: .5em">
-                    <td style="border: 0">Very Satisfactory</td>
-                    <td style="border: 0">85-89</td>
+                    <td style="border: 0">With High Honors</td>
+                    <td style="border: 0">95-97</td>
                     <td style="border: 0">Passed</td>                
                 </tr>
 
                 <tr style="margin-top: .5em">
-                    <td style="border: 0">Satisfactory</td>
-                    <td style="border: 0">80-84</td>
+                    <td style="border: 0">With Honors</td>
+                    <td style="border: 0">90-94</td>
                     <td style="border: 0">Passed</td>                
                 </tr>
 
                 <tr style="margin-top: .5em">
-                    <td style="border: 0">Fairly Satisfactory</td>
+                    <td style="border: 0">Passed</td>
                     <td style="border: 0">75-79</td>
                     <td style="border: 0">Passed</td>                
                 </tr>
 
                 <tr style="margin-top: .5em">
-                    <td style="border: 0">Did Not Meet expectations</td>
+                    <td style="border: 0">Failed</td>
                     <td style="border: 0">Below 75</td>
                     <td style="border: 0">Failed</td>                
                 </tr>
@@ -948,7 +950,7 @@
                 </tr>
 
                 <tr style="margin-top: .5em">
-                    <td colspan="3" style="border: 0">Lacking units in:__________________________</td>                
+                    <td colspan="3" style="border: 0">Lacking units in:___<u>{{ $Enrollment[0]->s2_lacking_unit }}</u>____</td>                
                 </tr>
                 
                 <tr style="margin-top: .5em">
@@ -1162,31 +1164,31 @@
                     </tr>
 
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Outstanding</td>
-                        <td style="border: 0">90-100</td>
+                        <td style="border: 0">With Highest Honors</td>
+                        <td style="border: 0">98-100</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
 
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Very Satisfactory</td>
-                        <td style="border: 0">85-89</td>
+                        <td style="border: 0">With High Honors</td>
+                        <td style="border: 0">95-97</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
 
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Satisfactory</td>
-                        <td style="border: 0">80-84</td>
+                        <td style="border: 0">With Honors</td>
+                        <td style="border: 0">90-94</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
 
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Fairly Satisfactory</td>
+                        <td style="border: 0">Passed</td>
                         <td style="border: 0">75-79</td>
                         <td style="border: 0">Passed</td>                
                     </tr>
 
                     <tr style="margin-top: .5em">
-                        <td style="border: 0">Did Not Meet expectations</td>
+                        <td style="border: 0">Failed</td>
                         <td style="border: 0">Below 75</td>
                         <td style="border: 0">Failed</td>                
                     </tr>
@@ -1212,7 +1214,7 @@
                     </tr>
 
                     <tr style="margin-top: .5em">
-                        <td colspan="3" style="border: 0">Lacking units in:__________________________</td>                
+                        <td colspan="3" style="border: 0">Lacking units in:___<u>{{ $Enrollment[0]->j_lacking_unit }}</u>____</td>                
                     </tr>
                     
                     <tr style="margin-top: .5em">
