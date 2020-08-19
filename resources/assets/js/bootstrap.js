62f5c8ad66e3dc9995475512347ebe6000e4830c
monroenikko <monroenikkoaldrich@yahoo.com>
2020-08-19 03:10:10 +0000

    add sample manual

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,6 +9,8 @@ window.Popper = require('popper.js').default;
  */
 
 try {
+    import $ from 'jquery';
+window.$ = window.jQuery = $;
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
